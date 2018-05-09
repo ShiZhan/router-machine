1d5c02e827b61364a43a00f8c21b3504a2bd17a6
Zhan.Shi <g.shizhan.g@gmail.com>
2018-05-09 21:29:47 +0800

    provision

--- provision.sh
+++ provision.sh
@@ -2,8 +2,12 @@
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 cp /home/vagrant/conf/sources.list /etc/apt/sources.list
 apt-get update && apt-get install -y iptables dnsmasq
+
+source /home/vagrant/conf/update-iptables.sh
+cp /home/vagrant/conf/update-iptables.sh /etc/network/if-up.d/update-iptables.sh
+service network restart
+
 cp /home/vagrant/conf/hosts-local /etc/hosts-local
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 cp /home/vagrant/conf/dnsmasq.conf /etc/dnsmasq.conf
 service dnsmasq restart
-source /home/vagrant/conf/update-iptables.sh
