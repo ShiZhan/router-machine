e17b61fd2218d0b6e7e58993c3fde14ef3932295
Zhan.Shi <g.shizhan.g@gmail.com>
2018-05-09 21:37:24 +0800

    update iptables

--- conf/update-iptables.sh
+++ conf/update-iptables.sh
@@ -1,6 +1,7 @@
-iptables --policy INPUT   ACCEPT
-iptables --policy OUTPUT  ACCEPT
-iptables --policy FORWARD ACCEPT
-iptables -Z && iptables -F && iptables -X
-iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
-iptables --append FORWARD --in-interface eth1 -j ACCEPT
+#!/bin/bash
+/sbin/iptables --policy INPUT   ACCEPT
+/sbin/iptables --policy OUTPUT  ACCEPT
+/sbin/iptables --policy FORWARD ACCEPT
+/sbin/iptables -Z && /sbin/iptables -F && /sbin/iptables -X
+/sbin/iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+/sbin/iptables --append FORWARD --in-interface eth1 -j ACCEPT
