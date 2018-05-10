f2e2ece9362aaabb75300a76d026e94f0470da72
Zhan <g.shizhan.g@gmail.com>
2018-05-10 21:28:59 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 cp /home/vagrant/conf/sources.list /etc/apt/sources.list
-apt-get update && apt-get install -y iptables dnsmasq
+apt-get update && apt-get install -y iptables iptables-persistent dnsmasq
 
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 source /home/vagrant/conf/update-iptables.sh
-cp /home/vagrant/conf/update-iptables.sh /etc/network/if-up.d/update-iptables.sh
-chmod +x /etc/network/if-up.d/update-iptables.sh
+service iptables-persistent save
 
 cp /home/vagrant/conf/hosts-local /etc/hosts-local
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
